9f3ec4bdc53763bdca9553b4935667bd8c7141a4
Michael Dominic K <mdk@me.com>
2009-02-10 21:25:53 +0800

    Adding a little bit of CSS styling.
    Signed-off-by: Phillip Toland <phil.toland@gmail.com>

--- markdown.m
+++ markdown.m
@@ -3,9 +3,20 @@
 
 NSData* renderMarkdown(NSURL* url)
 {
-    NSString *source = [NSString stringWithContentsOfFile:[url path]];
-    char *output = convert_markdown_to_string([source UTF8String]);
-    NSData *data = [NSData dataWithBytes:output length:strlen(output)];
+    NSString *styles = [NSString stringWithContentsOfFile:[[NSBundle bundleWithIdentifier: @"com.fiatdev.QLMarkdown"]
+                                                           pathForResource:@"styles" ofType:@"css"]];
     
-    return data;
+    NSString *source = [NSString stringWithContentsOfFile:[url path] encoding:NSUTF8StringEncoding error:nil];
+    char *output = convert_markdown_to_string([source UTF8String]);
+    NSString *html = [NSString stringWithFormat:@"<html>"
+                                                 "<head>"
+                                                 "<meta content='text/html; charset=UTF-8' http-equiv='Content-Type' />"
+                                                 "<style type=\"text/css\">%@</style>"
+                                                 "</head>"
+                                                 "<body>%@</body>"
+                                                 "</html>", 
+                                                 styles, [NSString stringWithUTF8String:output]];
+    
+    free(output);
+    return [html dataUsingEncoding:NSUTF8StringEncoding];
 }
