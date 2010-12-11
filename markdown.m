a7376e3e3c8a3029a9d3e08eef473462af763bc8
Nathan Sharfi <bonusfrogs@gmail.com>
2010-12-12 06:11:57 +0800

    automatically determine encoding of file instead of assuming UTF-8

--- markdown.m
+++ markdown.m
@@ -4,9 +4,19 @@
 NSData* renderMarkdown(NSURL* url)
 {
     NSString *styles = [NSString stringWithContentsOfFile:[[NSBundle bundleWithIdentifier: @"com.fiatdev.QLMarkdown"]
-                                                           pathForResource:@"styles" ofType:@"css"]];
+                                                           pathForResource:@"styles" ofType:@"css"]
+                                                 encoding:NSUTF8StringEncoding
+                                                    error:nil];
     
-    NSString *source = [NSString stringWithContentsOfFile:[url path] encoding:NSUTF8StringEncoding error:nil];
+    NSStringEncoding usedEncoding = 0;
+    NSError *e = nil;
+    
+    NSString *source = [NSString stringWithContentsOfURL:url usedEncoding:&usedEncoding error:&e];
+    
+    if (usedEncoding == 0) {
+        NSLog(@"Wasn't able to determine encoding for file “%@”", [url path]);
+    }
+
     char *output = convert_markdown_to_string([source UTF8String]);
     NSString *html = [NSString stringWithFormat:@"<html>"
                                                  "<head>"
