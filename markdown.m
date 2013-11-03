51559315c9c3063416f5679014a811eee1381fa6
Mathias Bynens <mathias@qiwi.be>
2013-11-03 09:20:26 +0100

    Simplify the generated HTML code

--- markdown.m
+++ markdown.m
@@ -7,27 +7,24 @@ NSData* renderMarkdown(NSURL* url)
                                                            pathForResource:@"styles" ofType:@"css"]
                                                  encoding:NSUTF8StringEncoding
                                                     error:nil];
-    
+
     NSStringEncoding usedEncoding = 0;
     NSError *e = nil;
-    
+
     NSString *source = [NSString stringWithContentsOfURL:url usedEncoding:&usedEncoding error:&e];
-    
+
     if (usedEncoding == 0) {
         NSLog(@"Wasn't able to determine encoding for file “%@”", [url path]);
     }
 
     char *output = convert_markdown_to_string([source UTF8String]);
-    NSString *html = [NSString stringWithFormat:@"<html>"
-                                                 "<head>"
-                                                 "<meta content='text/html; charset=UTF-8' http-equiv='Content-Type' />"
-                                                 "<style type=\"text/css\">%@</style>"
+    NSString *html = [NSString stringWithFormat:@"<!DOCTYPE html>"
+                                                 "<meta charset=utf-8>"
+                                                 "<style>%@</style>"
                                                  "<base href=\"%@\"/>"
-                                                 "</head>"
-                                                 "<body>%@</body>"
-                                                 "</html>", 
+                                                 "%@",
                                                  styles, url, [NSString stringWithUTF8String:output]];
-    
+
     free(output);
     return [html dataUsingEncoding:NSUTF8StringEncoding];
 }
